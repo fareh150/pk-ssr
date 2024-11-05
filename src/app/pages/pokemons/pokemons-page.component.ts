d429b5440e01378b11fd6f4a0c66dca3a9f1daaf
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-06 00:54:34 +0100

    list skeleton usage

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
 @Component({
   selector: 'pokemons-page',
   standalone: true,
-  imports: [PokemonListComponent],
+  imports: [
+    PokemonListComponent,
+    PokemonListSkeletonComponent
+  ],
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
