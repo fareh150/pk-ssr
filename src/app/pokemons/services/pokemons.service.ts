310ccf902a99ef48ef513f85f493a4874f6a2c82
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-06 17:39:12 +0100

    add loadPokemon to service

--- src/app/pokemons/services/pokemons.service.ts
+++ src/app/pokemons/services/pokemons.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable, tap } from 'rxjs';
-import { SimplePokemon, PokeAPIResponse } from '../interfaces';
+import { SimplePokemon, PokeAPIResponse, Pokemon } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -37,5 +37,11 @@ export class PokemonsService {
       }),
       //tap(console.log),
     );
+  }
+
+  public loadPokemon(id: string)
+  {
+    return this.http
+    .get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`);
   }
 }
