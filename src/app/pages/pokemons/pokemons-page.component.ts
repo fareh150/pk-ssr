1166e2ac6abf944b4eddea4634b2380ff3baffc6
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-06 01:19:11 +0100

    check isStable and applicate onDestroy for subscriptions

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
 import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
@@ -12,13 +12,24 @@ import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonsPageComponent implements OnInit {
-  public isLoading = signal(true)
+export default class PokemonsPageComponent implements OnInit, OnDestroy {
+  public isLoading = signal(true);
+
+  private appRef = inject(ApplicationRef);
+
+  private $appStable = this.appRef.isStable.subscribe( isStable =>
+  {
+    console.log({isStable});
+  });
 
   ngOnInit(): void {
     setTimeout(() => {
       this.isLoading.set(false)
     }, 5000)
+  }
+
+  ngOnDestroy(): void {
+    this.$appStable.unsubscribe();
   }
 
 
