8ab128fc8bc42745d27285e746de9e0b88ee9888
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-06 18:20:57 +0100

    add Title and metaTags

--- src/app/pages/pokemon-page/pokemon-page.component.ts
+++ src/app/pages/pokemon-page/pokemon-page.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@ang
 import { Pokemon } from '../../pokemons/interfaces/pokemon.interface';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pokemon-page',
@@ -13,6 +15,8 @@ import { ActivatedRoute } from '@angular/router';
 export default class PokemonPageComponent implements OnInit {
   private pokemonsService = inject(PokemonsService)
   private route = inject(ActivatedRoute)
+  private title = inject(Title)
+  private meta = inject(Meta)
 
   public pokemon = signal<Pokemon | null>(null)
 
@@ -20,6 +24,23 @@ export default class PokemonPageComponent implements OnInit {
     const id= this.route.snapshot.paramMap.get('id')
     if(!id) return
 
-    this.pokemonsService.loadPokemon(id).subscribe(this.pokemon.set)
+    this.pokemonsService.loadPokemon(id)
+      .pipe(
+        tap(({name, id}) =>
+          {
+            const pageTitle = `${id} - ${name}`;
+            const pageDescription = `Pagina del Pokémon ${name} Shiny`;
+
+            this.title.setTitle(pageTitle);
+            this.meta.updateTag({name: 'description', content: pageDescription});
+            this.meta.updateTag({name: 'og:title', content: pageTitle});
+            this.meta.updateTag({name: 'og:description', content: pageDescription});
+            this.meta.updateTag({
+              name: 'og:image',
+              content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${id}.png`
+            });
+          })
+      )
+      .subscribe(this.pokemon.set)
   }
 }
