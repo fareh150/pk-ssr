503a701efb7b109b9a3f34e45e3525bc403c3de0
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-07 20:31:45 +0100

    add pokemon-card and start settings
    -create a mockPokemon , we will need to send info for the input
    -add providers: provideRouter([]) to fix start router errors
    -use of ComponentRef to send data for  the component´s input
    -to use setInput, we send first the public name of the input in the component , and the second parameter will be my mockPokemon
    -to se changes activate fixture.detectChanges()

---
+++ src/app/pokemons/components/pokemon-card/pokemon-card.spec.ts
@@ -0,0 +1,38 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PokemonCardComponent } from './pokemon-card.component';
+import { provideRouter } from '@angular/router';
+import { SimplePokemon } from '../../interfaces';
+
+const mockPokemon: SimplePokemon = {
+  id: '1',
+  name: 'bulbasaur',
+}
+
+describe('PokemonCardComponent', () => {
+
+  let fixture: ComponentFixture<PokemonCardComponent>;
+  let compiled: HTMLElement;
+  let component: PokemonCardComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PokemonCardComponent],
+      providers: [provideRouter([])]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PokemonCardComponent);
+    // se pone primero el nombre del input en el componente y luego el valor creado (mock)
+    fixture.componentRef.setInput('pokemon', mockPokemon);
+
+    compiled = fixture.nativeElement as HTMLElement;
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    console.log(compiled);
+
+    expect(component).toBeTruthy();
+  });
+});
