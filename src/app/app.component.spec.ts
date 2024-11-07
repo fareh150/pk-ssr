9e27e3146a66190720ec88f15e5451523cd10ac2
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-07 17:09:10 +0100

    add mock and overrideComponent
    -agregamos un componente mock (falso / de pruebas)
    -remove al componente real
    -compilamos todo y revisamos
    -se hace para no re renderizar todo
    -cada componente tiene si test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
-let fixture: ComponentFixture<AppComponent>;
-let app: AppComponent;
-let compiled: HTMLDivElement;
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let compiled: HTMLDivElement;
+
+  @Component({
+    selector: 'app-navbar',
+    standalone: true,
+  })
+  class NavbarComponentMock {}
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [
         provideRouter([])
       ]
-    }).compileComponents();
+    })
+    .overrideComponent(AppComponent, {
+      add:{
+        imports: [NavbarComponentMock]
+      },
+      remove: {
+        imports: [NavbarComponent]
+      }
+    })
+    .compileComponents();
+    //.compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.componentInstance;
