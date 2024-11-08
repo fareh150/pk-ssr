91a73745ef26a74122dca97a821475a222658c1d
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-08 08:44:35 +0100

    [TEST] serviceSpect - http check  - pre settings

--- src/app/pokemons/services/pokemons.service.spec.ts
+++ src/app/pokemons/services/pokemons.service.spec.ts
@@ -1,10 +1,28 @@
 import { TestBed } from "@angular/core/testing";
 import { PokemonsService } from "./pokemons.service";
 import { provideHttpClient } from "@angular/common/http";
-import { provideHttpClientTesting } from "@angular/common/http/testing";
-import { SimplePokemon } from "../interfaces";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { PokeAPIResponse, SimplePokemon } from "../interfaces";
 
-const mockPokemons: SimplePokemon[] = [
+const mockPokeApiResponse: PokeAPIResponse = {
+
+    "count": 1302,
+    "next": "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+    "previous": null,
+    "results": [
+        {
+            "name": "bulbasaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/1/"
+        },
+        {
+            "name": "ivysaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/2/"
+        },
+    ]
+
+}
+
+const expectedPokemons: SimplePokemon[] = [
   {
     id: '1',
     name: 'bulbasaur',
@@ -23,6 +41,7 @@ const mockPokemon = {
 describe('PokemonsService', () =>
 {
   let service: PokemonsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() =>
   {
@@ -33,10 +52,32 @@ describe('PokemonsService', () =>
       ]
     });
     service = TestBed.inject(PokemonsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+  // despues de cada prueba http hay que verificar  y con afterEach no repetimos tanto codigo
+  afterEach(() =>
+  {
+    httpMock.verify();
   });
 
   it('should be created', () =>
   {
     expect(service).toBeTruthy();
   });
+
+  it('should load a page of a SimplePokemon',() =>
+  {
+    // subcribe to wait
+    service.loadPage(1).subscribe(pokemons =>
+    {
+      expect(pokemons).toEqual(expectedPokemons);
+    })
+    //create request to expected response
+    const req = httpMock
+      .expectOne(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
+
+    expect(req.request.method).toBe('GET');
+    // use false data
+    req.flush(mockPokeApiResponse)
+  })
 });
